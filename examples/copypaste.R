1764d06e0bbd1d2815c19fed4046839529102825
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-08-11 16:27:28 +0600

    example app for mod3

---
+++ examples/copypaste.R
@@ -0,0 +1,32 @@
+
+library(shiny)
+
+# Application
+
+ui <- fluidPage(
+  tags$h3("Import data from Global Environment"),
+  fluidRow(
+    column(
+      width = 4,
+      mod_import_copypaste_ui("myid")
+    ),
+    column(
+      width = 8,
+      tags$b("Imported data:"),
+      verbatimTextOutput(outputId = "result")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  imported <- callModule(mod_import_copypaste_server, "myid")
+  
+  output$result <- renderPrint({
+    imported$data()
+  })
+  
+}
+
+if (interactive())
+  shinyApp(ui, server)
