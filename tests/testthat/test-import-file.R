ba2b32431abd6bc37087d7da247f7f452c37fa64
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-09-21 20:14:59 +0600

    test for import-file

--- tests/testthat/test-import-file.R
+++ tests/testthat/test-import-file.R
@@ -1,3 +1,20 @@
 test_that("import_file_ui works", {
   expect_is(import_file_ui("ID"), "shiny.tag")
 })
+
+test_that("import_file_server works", {
+  shiny::testServer(import_file_server, {
+    session$setInputs(sheet = 0) #to bypass ignoreInit = TRUE
+    session$setInputs(
+      file = data.frame(
+        datapath = system.file("inst", "extdata", "mtcars.csv", package = "datamods")
+      ),
+      sheet = 0,
+      skip_rows = 0,
+      validate = 0
+    ) 
+    expect_is(imported_data$data, "data.frame")
+    expect_is(session$getReturned()$data(), "data.frame")
+  })
+})
+
