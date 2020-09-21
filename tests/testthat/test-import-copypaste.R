ccbaddd5200b6d7aff15dfb1727a04246e31939e
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-09-21 17:53:18 +0600

    added test for import_copypaste_server

--- tests/testthat/test-import-copypaste.R
+++ tests/testthat/test-import-copypaste.R
@@ -2,3 +2,15 @@ test_that("import_copypaste_ui works", {
   expect_is(import_copypaste_ui("ID"), "shiny.tag")
 })
 
+test_that("import_copypaste_server works", {
+  testServer(import_copypaste_server, {
+    session$setInputs(data_pasted = 0) #to bypass ignoreInit = TRUE
+    session$setInputs(
+      data_pasted = 
+      "x y z
+      1 2 3",
+      validate = 0
+    )
+    expect_is(imported_data$data, "data.table")
+  })
+})
