0749f03156576fabeb0a15ec05e5894f5648cffa
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-09-25 07:09:43 +0600

    import-modal functionality, done by victor, committed by me

--- R/import-modal.R
+++ R/import-modal.R
@@ -20,36 +20,15 @@
 #'
 #' @example examples/modal.R
 #' 
-import_ui <- function(id, from) {
+import_ui <- function(id, from = c("env", "file", "copypaste", "googlesheets", "database")) {
   ns <- NS(id)
-  tabsetPanel(
-    id = ns("tabs"),
-    selected = from,
-    tabPanel(
-      title = "From Environment",
-      value = "env",
-      import_globalenv_ui(ns("env"))
-    ),
-    tabPanel(
-      title = "From File",
-      value = "file",
-      import_file_ui(ns("file"))
-    ),
-    tabPanel(
-      title = "From Copy/Paste",
-      value = "copypaste",
-      import_copypaste_ui(ns("copypaste"))
-    ),
-    tabPanel(
-      title = "From Googlesheets",
-      value = "googlesheets",
-      import_googlesheets_ui(ns("googlesheets"))
-    ),
-    tabPanel(
-      title = "From Database",
-      value = "database"
-    )
-  )
+  from <- match.arg(from, several.ok = TRUE)
+  env <- if("env" %in% from) tabPanel("Env", import_globalenv_ui(ns("env")))
+  file <- if("file" %in% from) tabPanel("File", import_file_ui(ns("file")))
+  copypaste <- if("copypaste" %in% from) tabPanel("Copy/Paste", import_copypaste_ui(ns("copypaste")))
+  googlesheets <- if("googlesheets" %in% from) tabPanel("Googlesheets", import_googlesheets_ui(ns("googlesheets")))
+  #database <- if("database" %in% from) tabPanel("Database", import_database_ui(ns("database")))
+  do.call(tabsetPanel, dropNulls(list(env, file, copypaste, googlesheets)))
 }
 
 
@@ -58,34 +37,37 @@ import_ui <- function(id, from) {
 #' @importFrom shiny moduleServer reactiveValues observeEvent reactive
 #'
 #' @rdname import-modal
-import_server <- function(id,
-                          from = c("env", "file", "copypaste", "googlesheets", "database")) {
-  
-  from = match.arg(from)
+import_server <- function(id) {
   
   moduleServer(
     id,
     function(input, output, session){
-      ns <- session$ns
-      import_modal(id, from)
-
-      server_function <- reactiveValues(server = NULL)
       
-      observeEvent(input$tabs, {
-        from = input$tabs
-        server_selected <- switch(
-          from,
-          "env" = import_globalenv_server,
-          "file" = import_file_server,
-          "copypaste" = import_copypaste_server,
-          "googlesheets" = import_googlesheets_server
-        )
-        server_function$server <- server_selected(from)
+      data_rv <- reactiveValues(x = NULL)
+      
+      from_env <- import_globalenv_server("env")
+      from_file <- import_file_server("file")
+      from_copypaste <- import_copypaste_server("copypaste")
+      from_googlesheets <- import_googlesheets_server("googlesheets")
+      #from_database <- import_database_server("database")
+      
+      observeEvent(from_env$data(), {
+        data_rv$x <- from_env$data()
       })
+      observeEvent(from_file$data(), {
+        data_rv$x <- from_file$data()
+      })
+      observeEvent(from_copypaste$data(), {
+        data_rv$x <- from_copypaste$data()
+      })
+      observeEvent(from_googlesheets$data(), {
+        data_rv$x <- from_googlesheets$data()
+      })
+      # observeEvent(from_database$data(), {
+      #   data_rv$x <- from_database$data()
+      # })
       
-      return(list(
-        data = reactive(server_function$server$data())
-      ))
+      return(reactive(data_rv$x))
     }
   )
 }
@@ -96,4 +78,8 @@ import_server <- function(id,
 #' @importFrom shiny modalDialog showModal
 import_modal <- function(id, from) {
   showModal(modalDialog(import_ui(id, from)))
+}
+
+dropNulls <- function(x) {
+  x[!vapply(x, is.null, FUN.VALUE = logical(1))]
 }
--- examples/modal.R
+++ examples/modal.R
@@ -5,7 +5,14 @@ library(datamods)
 ui <- fluidPage(
   fluidRow(
     column(
-      width = 12,
+      width = 4,
+      checkboxGroupInput(inputId = "from",
+                         label = "From",
+                         choices = c("env", "file", "copypaste", "googlesheets")),
+      actionButton("launch_modal", "Launch Modal(s)")
+    ),
+    column(
+      width = 8,
       tags$b("Imported data:"),
       verbatimTextOutput(outputId = "result")
     )
@@ -14,10 +21,16 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  imported <- import_server("myid", "env")
+  observeEvent(input$launch_modal, {
+    req(input$from)
+    import_modal("myid", from = input$from)
+  })
+  
+  imported <- import_server("myid")
+  
   output$result <- renderPrint({
-    req(imported$data())
-    imported$data()
+    req(imported())
+    imported()
   })
 }
 
