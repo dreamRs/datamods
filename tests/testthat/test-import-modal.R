d0fd8e8f0447b270737603c7bd9c0139d9e5e7ab
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-10-28 10:10:50 +0600

    tests for import_server added, tried to test import_modal too but failed

--- tests/testthat/test-import-modal.R
+++ tests/testthat/test-import-modal.R
@@ -2,3 +2,15 @@ test_that("import_ui works", {
   expect_is(import_ui("ID"), "shiny.tag")
 })
 
+test_that("import_server works", {
+  shiny::testServer(import_server, args = list(return_class = "data.table"), {
+    data(mtcars)
+    session$env$data_rv$data = mtcars
+    session$env$data_rv$name = "mtcars"
+    
+    session$setInputs(validate = 1)
+    
+    expect_is(session$getReturned()$data(), "data.table")
+    expect_equal(session$getReturned()$name(), "mtcars")
+  })
+})
