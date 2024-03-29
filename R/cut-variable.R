8fe00807940255edbd65b4ac524a54e5e3168836
pvictor <perrier.victor@gmail.com>
2024-03-29 15:12:53 +0100

    added module cut_variable

---
+++ R/cut-variable.R
@@ -0,0 +1,200 @@
+
+#' @title Module to Convert Numeric to Factor
+#'
+#' @description
+#' This module contain an interface to cut a numeric factor into several intervals.
+#'
+#'
+#' @param id Module ID.
+#'
+#' @return A [shiny::reactive()] function returning the data.
+#' @export
+#'
+#' @importFrom shiny NS fluidRow column numericInput checkboxInput checkboxInput plotOutput
+#' @importFrom shinyWidgets virtualSelectInput
+#' @importFrom toastui datagridOutput2
+#'
+#' @name cut-variable
+#'
+#' @example examples/cut_variable.R
+cut_variable_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    fluidRow(
+      column(
+        width = 3,
+        virtualSelectInput(
+          inputId = ns("variable"),
+          label = "Variable to cut:",
+          choices = NULL,
+          width = "100%"
+        )
+      ),
+      column(
+        width = 3,
+        virtualSelectInput(
+          inputId = ns("method"),
+          label = "Method:",
+          choices = c(
+            "sd", "equal", "pretty", "quantile", "kmeans",
+            "hclust", "bclust", "fisher", "jenks", "headtails", "maximum", "box"
+          ),
+          selected = "quantile",
+          width = "100%"
+        )
+      ),
+      column(
+        width = 3,
+        numericInput(
+          inputId = ns("n_breaks"),
+          label = "Number of breaks:",
+          value = 5,
+          min = 2,
+          max = 12,
+          width = "100%"
+        )
+      ),
+      column(
+        width = 3,
+        checkboxInput(
+          inputId = ns("right"),
+          label = "Close intervals on the right",
+          value = TRUE
+        ),
+        checkboxInput(
+          inputId = ns("include_lowest"),
+          label = "Include lowest value",
+          value = FALSE
+        )
+      )
+    ),
+    plotOutput(outputId = ns("plot"), width = "100%"),
+    datagridOutput2(outputId = ns("count"))
+  )
+}
+
+#' @export
+#'
+#' @importFrom shiny moduleServer observeEvent reactive req bindEvent
+#' @importFrom shinyWidgets updateVirtualSelect
+#' @importFrom toastui renderDatagrid2 datagrid grid_colorbar
+#'
+#' @rdname cut-variable
+cut_variable_server <- function(id, data_r = reactive(NULL)) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+
+      bindEvent(observe({
+        data <- data_r()
+        vars_num <- vapply(data, is.numeric, logical(1))
+        vars_num <- names(vars_num)[vars_num]
+        updateVirtualSelect(
+          inputId = "variable",
+          choices = vars_num,
+          selected = vars_num[1]
+        )
+      }), data_r(), input$hidden)
+
+      breaks_r <- reactive({
+        data <- req(data_r())
+        variable <- req(input$variable)
+        req(hasName(data, variable))
+        req(input$n_breaks, input$method)
+        classInt::classIntervals(data[[variable]], n = input$n_breaks, style = input$method)
+      })
+
+      output$plot <- renderPlot({
+        data <- req(data_r())
+        variable <- req(input$variable)
+        # ggplot_histogram(data, variable, breaks = breaks_r()$brks)
+        plot_histogram(data, variable, breaks = breaks_r()$brks)
+      })
+
+
+      data_cutted_r <- reactive({
+        data <- req(data_r())
+        variable <- req(input$variable)
+        data[[paste0(variable, "_cut")]] <- cut(
+          x = data[[variable]],
+          breaks = breaks_r()$brks,
+          include.lowest = input$include_lowest,
+          right = input$right
+        )
+        data
+      })
+
+      output$count <- renderDatagrid2({
+        data <- req(data_cutted_r())
+        variable <- req(input$variable)
+        count_data <- as.data.frame(
+          table(
+            breaks = data[[paste0(variable, "_cut")]]
+          ),
+          responseName = "count"
+        )
+        datagrid(
+          data = count_data,
+          colwidths = "guess",
+          theme = "striped",
+          bodyHeight = "auto"
+        ) %>%
+          grid_colorbar(
+            column = "count",
+            label_outside = TRUE,
+            label_width = "40px",
+            bar_bg = "#112466",
+            from = 0
+          )
+      })
+
+      return(data_cutted_r)
+    }
+  )
+}
+
+
+
+#' @inheritParams shiny::modalDialog
+#' @export
+#'
+#' @importFrom shiny showModal modalDialog textInput
+#' @importFrom htmltools tagList
+#'
+#' @rdname cut-variable
+modal_cut_variable <- function(id,
+                               title = "Convert Numeric to Factor",
+                               easyClose = TRUE,
+                               size = "l",
+                               footer = NULL) {
+  ns <- NS(id)
+  showModal(modalDialog(
+    title = tagList(title, button_close_modal()),
+    cut_variable_ui(id),
+    tags$div(
+      style = "display: none;",
+      textInput(inputId = ns("hidden"), label = NULL, value = genId())
+    ),
+    easyClose = easyClose,
+    size = size,
+    footer = footer
+  ))
+}
+
+
+
+plot_histogram <- function(data, column, bins = 30, breaks = NULL) {
+  x <- data[[column]]
+  op <- par(mar = rep(1.5, 4)); on.exit(par(op))
+  plot.new()
+  plot.window(xlim = range(pretty(x)), ylim =  range(pretty(hist(x, breaks = bins, plot = FALSE)$counts)))
+  abline(v = pretty(x), col = "#D8D8D8")
+  abline(h = pretty(hist(x, breaks = bins, plot = FALSE)$counts), col = "#D8D8D8")
+  hist(x, breaks = bins, xlim = range(pretty(x)), xaxs = "i", yaxs = "i", col = "#112466", add = TRUE)
+  axis(side = 1, at = pretty(x), pos = 0)
+  axis(side = 2, at = pretty(hist(x, breaks = bins, plot = FALSE)$counts), pos = min(pretty(x)))
+  abline(v = breaks, col = "#FFFFFF", lty = 1, lwd = 1.5)
+  abline(v = breaks, col = "#2E2E2E", lty = 2, lwd = 1.5)
+}
+
+
---
+++ examples/cut_variable.R
@@ -0,0 +1,59 @@
+
+library(shiny)
+library(datamods)
+library(reactable)
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(version = 5L, preset = "bootstrap"),
+  tags$h2("Convert Numeric to Factor"),
+  fluidRow(
+    column(
+      width = 6,
+      cut_variable_ui("inline"),
+      actionButton("modal", "Or click here to open a modal to cut a variable")
+    ),
+    column(
+      width = 6,
+      reactableOutput(outputId = "table"),
+      verbatimTextOutput("code")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+
+  rv <- reactiveValues(data = MASS::Cars93[, c(1, 3, 4, 5, 6, 10)])
+
+  # inline mode
+  data_inline_r <- cut_variable_server(
+    id = "inline",
+    data_r = reactive(rv$data)
+  )
+  observeEvent(data_inline_r(), rv$data <- data_inline_r())
+
+  # modal window mode
+  observeEvent(input$modal, modal_cut_variable("modal"))
+  data_modal_r <- cut_variable_server(
+    id = "modal",
+    data_r = reactive(rv$data)
+  )
+  observeEvent(data_modal_r(), rv$data <- data_modal_r())
+
+  # Show result
+  output$table <- renderReactable({
+    data <- req(rv$data)
+    reactable(
+      data = data,
+      bordered = TRUE,
+      compact = TRUE,
+      striped = TRUE
+    )
+  })
+
+  output$code <- renderPrint({
+    attr(rv$data, "code")
+  })
+}
+
+if (interactive())
+  shinyApp(ui, server)
