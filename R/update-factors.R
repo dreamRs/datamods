e4a70796555ee077e06471132891c8ea5fd0688f
gsamra <samra.goumri@dreamrs.fr>
2024-04-09 16:53:23 +0200

    update-factors : sort levels and number of occurrences

--- R/update-factors.R
+++ R/update-factors.R
@@ -26,13 +26,25 @@ update_factors_ui <- function(id) {
         width = 3,
         virtualSelectInput(
           inputId = ns("variable"),
-          label = "Factor variable levels to reorder:",
+          label = "Factor variable to reorder:",
           choices = NULL,
           width = "100%"
         )
       ),
       column(
         width = 9,
+        # radioGroupButtons(
+        #   inputId = ns("sort"),
+        #   label = "Sort:",
+        #   choices = c(
+        #     "Increasing levels" = "increasing_levels", 
+        #     "Decreasing levels" = "decreasing_levels",
+        #     "Increasing occurrences" = "increasing_occurrences", 
+        #     "Decreasing occurrences" = "decreasing_occurrences"
+        #   ),
+        #   selected = "increasing_levels",
+        #   width = "100%"
+        # ),
         datagridOutput2(ns("grid"))
       )
     ),
@@ -63,7 +75,7 @@ update_factors_server <- function(id, data_r = reactive(NULL)) {
     id,
     function(input, output, session) {
       
-      rv <- reactiveValues(data = NULL)
+      rv <- reactiveValues(data = NULL) # data_grid = NULL
 
       bindEvent(observe({
         data <- data_r()
@@ -77,14 +89,45 @@ update_factors_server <- function(id, data_r = reactive(NULL)) {
         )
       }), data_r())
       
+      data_updated_r <- reactive({
+        data <- req(data_r())
+        variable <- req(input$variable)
+        data_grid <- req(input$grid_data)
+        data[[paste0(variable, "_updated")]] <- fct_relevel(
+          data[[variable]],
+          levels(fct_inorder(as.factor(data_grid[["Var1"]])))
+        )
+        data
+      })
+      
       output$grid <- renderDatagrid2({
         data_r <- data_r()
+        variable <- req(input$variable)
+        data_grid <- as.data.frame(table(data_r[[variable]]))
+
+        # if (input$sort == "increasing_levels") {
+        #   rv$data_grid <- data_grid %>%
+        #     arrange(Var1)
+        # }
+        # if (input$sort == "decreasing_levels") {
+        #   rv$data_grid <- data_grid %>%
+        #     arrange(desc(Var1))
+        # } else if (input$sort == "increasing_occurrences") {
+        #   rv$data_grid <- data_grid %>%
+        #     arrange(Freq)
+        # } else if (input$sort == "decreasing_occurrences"){
+        #   rv$data_grid <- data_grid %>%
+        #     arrange(desc(Freq))
+        # } else {
+        #   NULL
+        # }
+        
         datagrid(
-          data =  as.data.frame(table(data_r[[input$variable]])),
+          data =  data_grid, #rv$data_grid
           draggable = TRUE,
-          sortable = FALSE,
+          # sortable = FALSE,
           data_as_input = TRUE
-        ) %>% 
+        ) %>%
           grid_columns(
             columns = c("Var1", "Freq"),
             header = c("Levels", "Number of occurences")
@@ -102,16 +145,6 @@ update_factors_server <- function(id, data_r = reactive(NULL)) {
         input$grid_data
       })
       
-      data_updated_r <- reactive({
-        data <- req(data_r())
-        variable <- req(input$variable)
-        data_grid <- req(input$grid_data)
-        data[[paste0(variable, "_updated")]] <- fct_relevel(
-          data[[variable]],
-          levels(fct_inorder(as.factor(data_grid[["Var1"]])))
-        )
-        data
-      })
       
       data_returned_r <- observeEvent(input$create, {
         rv$data <- data_updated_r()
