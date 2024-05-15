a21bea050c52c36c563f2f5766bc7f25d9d85f84
gsamra <samra.goumri@dreamrs.fr>
2024-05-15 13:41:39 +0200

    maj script maj_csv

--- inst/i18n/maj_csv.R
+++ inst/i18n/maj_csv.R
@@ -12,17 +12,16 @@ source("inst/i18n/extract_labels.R")
 extraction_labels <- c(extract_labels(folder = "R"), extract_labels(folder = "examples"))
 
 
-# Mise à jour des csv avec les labels extraits précédemment SANS traduction
-
-update_csv(labels = extraction_labels)
-
-
 # Mise à jour des csv avec les labels extraits précédemment AVEC traduction
 
-update_csv(labels = extraction_labels, translation = TRUE)
-
-
-update_csv(labels = extraction_labels, lang = "fr", ....)
-update_csv(labels = extraction_labels, lang = "es", ....)
-update_csv(labels = extraction_labels, lang = "ja", encoding = "ISO_2022,locale=ja,version=4")
-update_csv(labels = extraction_labels, lang = "fr", ....)
+update_csv(labels = extraction_labels, lang = "fr")
+update_csv(labels = extraction_labels, lang = "es")
+update_csv(labels = extraction_labels, lang = "de")
+update_csv(labels = extraction_labels, lang = "sq")
+update_csv(labels = extraction_labels, lang = "pl")
+update_csv(labels = extraction_labels, lang = "pt")
+update_csv(labels = extraction_labels, lang = "tr")
+update_csv(labels = extraction_labels, lang = "mk")
+# update_csv(labels = extraction_labels, lang = "ja", encoding = "ISO_2022,locale=ja,version=4")  # revoir encoding
+update_csv(labels = extraction_labels, lang = "zh-CN", encoding = "GBK") #"UTF16_BigEndian" or "GB18030"
+update_csv(labels = extraction_labels, lang = "ko", encoding = "korean") # "GBK", "korean"
